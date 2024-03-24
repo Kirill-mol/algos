983052294089d11b19a9b628161a1628cfc3f522
Мололкин Кирилл <kmololkin@alfabank.ru>
2024-03-24 12:15:58 +0300

    Tasks for intervals

---
+++ arrays sorting/228. Summary Ranges/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        if (nums.length == 0) {
+            return Collections.emptyList();
+        }
+
+        List<String> ranges = new ArrayList(nums.length);
+        int curRangeStart = nums[0];
+        int curRangeEnd = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            if (nums[i] == curRangeEnd + 1) {
+                curRangeEnd = nums[i];
+            } else {
+                ranges.add(getRange(curRangeStart, curRangeEnd));
+                curRangeStart = nums[i];
+                curRangeEnd = nums[i];
+            }
+        }
+        ranges.add(getRange(curRangeStart, curRangeEnd));
+        return ranges;
+    }
+
+    public String getRange(int start, int end) {
+        if (start == end) {
+            return Integer.toString(start);
+        } else {
+            return "%d->%d".formatted(start, end);
+        }
+    }
+}
---
+++ arrays sorting/56. Merge Intervals/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int[][] resultIntervals = new int[intervals.length][2];
+        int[] curResultInterval = intervals[0];
+        int resultIndex = 0;
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] curInterval = intervals[i];
+            if (curInterval[0] <= curResultInterval[1]) {
+                curResultInterval[1] = Integer.max(curInterval[1], curResultInterval[1]);
+            } else {
+                resultIntervals[resultIndex++] = curResultInterval;
+                curResultInterval = curInterval;
+            }
+        }
+        resultIntervals[resultIndex++] = curResultInterval;
+
+        return Arrays.copyOfRange(resultIntervals, 0, resultIndex);
+    }
+}
